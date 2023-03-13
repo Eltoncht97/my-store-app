638ae38bae5192b66c26c37a962f11766dab9b42
Elton Chavez <elton.mct@gmail.com>
2023-03-13 02:20:14 -0300

    roles users options updated

--- src/modules/users/views/UserCreate.vue
+++ src/modules/users/views/UserCreate.vue
@@ -36,7 +36,7 @@
                 <input
                   id="vue-checkbox-list"
                   type="checkbox"
-                  value="admin"
+                  value="gerente"
                   class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                   v-model="userForm.roles"
                 />
