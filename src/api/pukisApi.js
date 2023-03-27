8c36462d83630de578801cf7870b75a29914f882
Elton Chavez <elton.mct@gmail.com>
2023-03-27 12:21:44 -0300

    fix pukis api url

--- src/api/pukisApi.js
+++ src/api/pukisApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const pukisApi = axios.create({
-  // baseURL: "https://pukis.herokuapp.com/api",
-  baseURL: 'http://localhost:3000/api'
+  baseURL: "https://pukis.herokuapp.com/api",
+  // baseURL: 'http://localhost:3000/api'
 });
 
 pukisApi.interceptors.request.use((config) => {
